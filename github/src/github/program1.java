368dc6f44924c4c83f1f4ba974300605e2bdf4c1
hp <hp@MAAZ>
2022-05-16 14:25:28 +0530

    added with on more steps

--- github/src/github/program1.java
+++ github/src/github/program1.java
@@ -4,6 +4,7 @@ public class program1 {
 	public static void main(String[] args) {
 	    System.out.println("Hello everyone, I am MOHD AHSAN");
 	    System.out.println("I am java developer");
+	   System.out.println("Welcome to all");
 	  }
 
 }
