897c72a77d6ac370f93bb44f83284174855836a2
hp <hp@MAAZ>
2022-05-16 14:18:20 +0530

    Ahsan completed the first stage

---
+++ github/src/github/program1.java
@@ -0,0 +1,9 @@
+package github;
+
+public class program1 {
+	public static void main(String[] args) {
+	    System.out.println("Hello everyone, I am MOHD AHSAN");
+	    System.out.println("I am java developer");
+	  }
+
+}
